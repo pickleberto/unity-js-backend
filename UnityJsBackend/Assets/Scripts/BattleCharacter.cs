34bbb00858e6a7daa50c44ffa4c0cf4994cd88ff
Alberto Sousa <albertovssousa@gmail.com>
2024-03-20 11:55:22 -0300

    display health and mana as text

--- UnityJsBackend/Assets/Scripts/BattleCharacter.cs
+++ UnityJsBackend/Assets/Scripts/BattleCharacter.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Newtonsoft.Json.Linq;
+using TMPro;
 
 public class BattleCharacter : MonoBehaviour
 {
     [SerializeField] private Image facePic;
+    
+    [Header("Health")]
     [SerializeField] private Image healthBar;
+    [SerializeField] private TMP_Text healthText;
+    [Header("Mana")]
     [SerializeField] private Image manaBar;
+    [SerializeField] private TMP_Text manaText;
 
     private readonly int MAX_HEALTH = 100;
     private readonly int MAX_MANA = 100;
@@ -17,6 +23,9 @@ public class BattleCharacter : MonoBehaviour
     {
         healthBar.fillAmount = (float)data["character"]["health"] / MAX_HEALTH;
         manaBar.fillAmount = (float)data["character"]["mana"] / MAX_MANA;
+
+        healthText.text = data["character"]["health"].ToString();
+        manaText.text = data["character"]["mana"].ToString();
 
         StartCoroutine(Utils.LoadTexture(data["character"]["imageUrl"].ToString(), facePic));
     }
